d19ed610ac6d2c89b4034600955677ab5470c8c6
Syhaun <2776299759@qq.com>
2023-11-09 23:59:24 +0800

    updata

---
+++ temp/单链表建立 节点删除(2).cpp
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<malloc.h>
+typedef struct student
+{
+	long num;
+	char name[100];
+	char sex[100];
+	int age;
+	struct student* next; 
+} LNode, *LinkList;
+//创建链表(包含了初始化） 
+void creat(LinkList H)
+{
+	H = (LinkList)malloc(sizeof(LNode));  
+	H -> next = NULL;  //创建头结点
+	LNode *r = H;
+	LinkList s = (LinkList)malloc(sizeof(LNode));
+
+	while(s->num!=-1)
+	{
+		s=(LNode*)malloc(sizeof(LNode));
+		r->next=s;
+		r=s;
+		scanf("%ld%s%s%d",&s->num,s->name,s->sex,&s->age);
+	}
+	r->next=NULL;
+}
+void del(LinkList H, int x)
+{
+	LinkList p,q;          //p指向第i-1的位置 q指向i的位置 
+	p=q=(LNode*)malloc(sizeof(LNode));
+	p=H;
+	int j=0;
+	while(j<x-1&&p)
+	{
+		p=p->next;
+		j++;
+	} 
+	if(p==NULL||p->next==NULL)
+	  printf("结点不存在！");
+	else
+	{
+		q=p->next;
+		p->next=q->next;
+		free(q);
+	 } 
+}
+void print(LinkList H)
+{
+	LNode* p;
+	p = H->next;
+	while (p)
+	{
+		printf(" % ld % s % s % d", p->num, p->name, p->sex, p->age);
+		p = p->next;
+	}
+}
+int main()
+{
+	LinkList H;
+	printf("请输入学号、姓名、性别、年龄\n");
+	creat(H);
+	print(H);
+	printf("请输入要删除的年龄\n");
+	int x;
+	scanf("%d", &x);
+	del(H,x);
+	print(H);
+	return 0;
+}
--- temp/哈夫曼编码器.cpp
+++ temp/哈夫曼编码器.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #define MAX_SIZE 256
+
 typedef struct Node
 {
     char data;
@@ -9,7 +10,8 @@ typedef struct Node
     struct Node *left;
     struct Node *right;
 } Node;
-Node* createNode(char data,int frequency)
+
+Node *createNode(char data, int frequency)
 {
     Node *node = (Node *)malloc(sizeof(Node));
     node->data = data;
@@ -18,19 +20,19 @@ Node* createNode(char data,int frequency)
     node->right = NULL;
     return node;
 }
-Node* buildHuffmanTree(char data[],int frequency[],int size)
+
+Node *buildHuffmanTree(char data[], int frequency[], int size)
 {
     Node **leafNodes = (Node **)malloc(size * sizeof(Node *));
     for (int i = 0; i < size; i++)
     {
         leafNodes[i] = createNode(data[i], frequency[i]);
     }
-    while(size > 1)
+    while (size > 1)
     {
         int min1 = 0;
         int min2 = 1;
-        if(leafNodes[min1] -> frequency > leafNodes[min2]
-        -> frequency)
+        if (leafNodes[min1]->frequency > leafNodes[min2]->frequency)
         {
             int temp = min1;
             min1 = min2;
@@ -38,30 +40,30 @@ Node* buildHuffmanTree(char data[],int frequency[],int size)
         }
         for (int i = 2; i < size; i++)
         {
-            if(leafNodes[i] -> frequency < leafNodes[min1]->frequency)
+            if (leafNodes[i]->frequency < leafNodes[min1]->frequency)
             {
                 min2 = min1;
                 min1 = i;
             }
-            else if (leafNodes[i]->frequency < leafNodes[min2] ->frequency)
+            else if (leafNodes[i]->frequency < leafNodes[min2]->frequency)
             {
                 min2 = i;
             }
         }
         Node *inNode = createNode('$', leafNodes[min1]->frequency + leafNodes[min2]->frequency);
-            inNode->left = leafNodes[min1];
-            inNode->right = leafNodes[min2];
-            leafNodes[min1] = inNode;
-
-            for (int i = min2; i < size - 1; i++)
-                leafNodes[i] = leafNodes[i + 1];
-            size--;
+        inNode->left = leafNodes[min1];
+        inNode->right = leafNodes[min2];
+        leafNodes[min1] = inNode;
+        for (int i = min2; i < size - 1; i++)
+            leafNodes[i] = leafNodes[i + 1];
+        size--;
     }
     Node *root = leafNodes[0];
     free(leafNodes);
     return root;
 }
-void writeEncodedTextToFile(const char *filename, const char *encodedText)
+
+void writeEncodedTextToFile(const char *filename,const char *encodedText)
 {
     FILE *file = fopen(filename, "w");
     if (file == NULL)
@@ -69,8 +71,124 @@ void writeEncodedTextToFile(const char *filename, const char *encodedText)
         printf("文件打开失败!\n");
         return;
     }
-    fputs(encodedText, file);
+    size_t len = strlen(encodedText);
+    fwrite(encodedText, sizeof(char), len, file);
     fclose(file);
+}
+void printTree(Node *root, int space)
+{
+    if (root == NULL)
+        return;
+
+    space += 3;
+
+    printTree(root->right, space);
+
+    printf("\n");
+    for (int i = 1; i < space; i++)
+        printf(" ");
+    printf("%c\n", root->data);
+
+    printTree(root->left, space);
+}
+
+void printHuffmanCodesHelper(Node *root, char *code, int depth)
+{
+    if (root->left == NULL && root->right == NULL)
+    {
+        code[depth] = '\0';
+        printf("%c: %s\n", root->data, code);
+        return;
+    }
+
+    if (root->left != NULL)
+    {
+        code[depth] = '0';
+        printHuffmanCodesHelper(root->left, code, depth + 1);
+    }
+
+    if (root->right != NULL)
+    {
+        code[depth] = '1';
+        printHuffmanCodesHelper(root->right, code, depth + 1);
+    }
+}
+
+void getHuffmanCodes(Node *root)
+{
+    char *code = (char *)malloc(256 * sizeof(char));
+    printHuffmanCodesHelper(root, code, 0);
+    free(code);
+}
+
+char *getHuffmanCode(Node *root, char ch, char *code)
+{
+    if (root == NULL)
+        return NULL;
+
+    if (root->left == NULL && root->right == NULL)
+    {
+        if (root->data == ch)
+        {
+            code[0] = '\0';
+            return code;
+        }
+        else
+        {
+            return NULL;
+        }
+    }
+
+    if (root->left != NULL)
+    {
+        code[0] = '0';
+        char *result = getHuffmanCode(root->left, ch, code + 1);
+        if (result != NULL)
+            return result;
+    }
+
+    if (root->right != NULL)
+    {
+        code[0] = '1';
+        char *result = getHuffmanCode(root->right, ch, code + 1);
+        if (result != NULL)
+            return result;
+    }
+
+    return NULL;
+}
+void decodeText(Node *root)
+{
+    FILE *codeFile = fopen("D://代码文件夹集//c语言//temp//CodeFile.txt", "r");
+    FILE *decodeFile = fopen("D://代码文件夹集//c语言//temp//DecodeFile.txt", "w");
+    if (codeFile == NULL || decodeFile == NULL)
+    {
+        printf("Failed to open the file.\n");
+        exit(1);
+    }
+
+    Node *node = root;
+    int c;
+    while ((c = fgetc(codeFile)) != EOF)
+    {
+        if (c == '0')
+        {
+            node = node->left;
+        }
+        else if (c == '1')
+        {
+            node = node->right;
+        }
+
+        if (node->left == NULL && node->right == NULL)
+        {
+            fprintf(decodeFile, "%c", node->data);
+            node = root;
+        }
+    }
+
+    fclose(codeFile);
+    fclose(decodeFile);
 }
 int main()
 {
@@ -95,13 +213,38 @@ int main()
     for (int i = 0; i < MAX_SIZE; i++)
     {
         if (charFrequency[i] > 0)
-        { 
-            data[i] = i;
-            frequency[i] = charFrequency[i];
-            printf("字符: %c  频率: %d\n",data[i],frequency[i]);
+        {
+            data[size] = i;
+            frequency[size] = charFrequency[i];
+            printf("字符: %c  频率: %d\n", data[size], frequency[size]);
+            size++;
         }
-        size++;
     }
     Node *root = buildHuffmanTree(data, frequency, size);
-    return 0;
+    //printTree(root, 0);
+    getHuffmanCodes(root);
+    // 对正文进行编码并存入文件
+    char encodedText[10000] = ""; // 用于存储编码后的文本
+    FILE *inputFile = fopen("D:\\代码文件夹集\\c语言\\temp\\SourceFile.txt", "r");
+    if (inputFile == NULL)
+    {
+        printf("文件打开失败!\n");
+        return 1;
+    }
+    decodeText(root);
+    int i = 0;
+    char code[100];
+    while ((ch = fgetc(inputFile)) != EOF)
+    {
+        getHuffmanCode(root, ch, code);
+        //printf("%d",i++);
+        strcat(encodedText, code);
+ //       printf("%s", encodedText);
+    }
+    //printf("%codes" );
+    fclose(inputFile);
+    // printf("%s", code);
+     //printf("%s", encodedText);
+     writeEncodedTextToFile("D://代码文件夹集//c语言//temp//CodeFile.txt", encodedText);
+     return 0;
 }
