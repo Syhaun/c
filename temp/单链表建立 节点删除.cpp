45b21b5a20cf4d8be402159af92d6593253b10eb
Syhaun <2776299759@qq.com>
2023-11-12 21:29:05 +0800

    updata

--- temp/单链表建立 节点删除.cpp
+++ temp/单链表建立 节点删除.cpp
@@ -10,59 +10,63 @@ typedef struct student
 	struct student* next; 
 } LNode, *LinkList;
 //创建链表(包含了初始化） 
-LinkList creat(LinkList H)
+LinkList creat()
 {
-	H = (LinkList)malloc(sizeof(LNode));  
+	LinkList H = (LinkList)malloc(sizeof(LNode));  
 	H -> next = NULL;  //创建头结点
 	LNode *r = H;
 	LinkList s = (LinkList)malloc(sizeof(LNode));
-	while(s->num!=-1)
+
+	while(1)
 	{
-		s=(LNode*)malloc(sizeof(LNode));
 		scanf("%ld%s%s%d",&s->num,s->name,s->sex,&s->age);
+		if(s->num==-1)
+		{
+			free(s);
+			break;
+		}
 		r->next=s;
 		r=s;
+		s=(LNode*)malloc(sizeof(LNode));
 	}
 	r->next=NULL;
 	return H;
 }
 void del(LinkList H, int x)
 {
-	LinkList p,q;          //p指向第i-1的位置 q指向i的位置 
-	p=q=(LNode*)malloc(sizeof(LNode));
-	p=H;
-	int j=0;
-	while(j<x-1&&p)
+	LinkList t, p = H->next, pre = H;
+	while (p)
 	{
-		p=p->next;
-		j++;
-	} 
-	if(p==NULL||p->next==NULL)
-	  printf("结点不存在！");
-	else
-	{
-		q=p->next;
-		p->next=q->next;
-		free(q);
-	 } 
+		while (p && p->age != x)
+		{
+			p = p->next;
+			pre = pre->next;
+		}
+		if (p)
+		{
+			t = p;
+			pre->next = p->next;
+			p = p->next; // 保持 p 不变，只更新 pre
+			free(t);
+		}
+	}
 }
 void print(LinkList H)
 {
 	LNode* p;
-	p = H -> next;
+	p = H->next;
 	while (p)
 	{
-		printf(" % ld % s % s % d", p->num, p->name, p->sex, p->age);
+		printf(" % ld % s % s % d\n", p->num, p->name, p->sex, p->age);
 		p = p->next;
 	}
 }
 int main()
 {
-	LinkList Head;
 	printf("请输入学号、姓名、性别、年龄\n");
-	LinkList H = creat(Head);
+	LinkList H = creat();
 	print(H);
-	printf("请输入要删除的年龄\n");
+	printf("请输入要删除的年龄\n");     //按值删除 
 	int x;
 	scanf("%d", &x);
 	del(H,x);
